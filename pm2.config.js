1aa74f2c09ad3f85675ede09500368f645d3afef
Jesús Bintaned <jesus@10labs.es>
2023-06-01 01:58:31 +0200

    correccion de errores

--- pm2.config.js
+++ pm2.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apps: [
     {
       name: 'tfm-malware-api',
-      script: 'node /tfm-malware-api/src/apps/index.js',
+      script: 'node /tfm-malware-api/src/apps/malware-api/index.js',
       error_file: './pm2-logs/tfm-malware-api-err.log',
       out_file: './pm2-logs/tfm-malware-api-output.log'
     }
