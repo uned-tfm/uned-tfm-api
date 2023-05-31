7374e8d085107f39c870083ec72be8faf047795f
Jesús Bintaned <jesus@10labs.es>
2023-06-01 01:01:08 +0200

    Añadido fichero de nginx

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,19 @@ if [ $# -lt 1 ]; then
 fi
 
 if [ $1 == "docker_down" ]; then
-  docker-compose -f docker-compose.yml down
+  docker-compose -f ./deployment/docker-compose.yml down
   exit 1
 fi
 
 if [ $1 == "docker_up" ]; then
   # 1. Creamos el contenedor de node 16.10 y compilamos el proyecto.
-  docker run --name tfm_malware_node -v $(pwd)/../:/tfm_malware -it node:16.10 bash -c "sh /tfm_malware/tfm-malware.build.sh"
+  docker run --name tfm_malware_node -v $(pwd):/tfm_malware -it node:16.10 bash -c "sh /tfm_malware/tfm-malware.build.sh"
 
   # 2. Eliminamos el contenedor y la imagen.
   docker rm tfm_malware_node
   docker rmi node:16.10
 
   # 3. Desplegamos el proyecto.
-  docker-compose -f docker-compose.yml up --build -d
+  docker-compose -f ./deployment/docker-compose.yml up --build -d
   exit 1
 fi
